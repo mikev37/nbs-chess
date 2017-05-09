2a16b00e58a0494f6cc7ff248e07c006dadf057f
Mikhail Vorontsov <mi17@umbc.edu>
2017-05-09 14:18:58 +0000

    Refactoring LogInPanel out of app file

--- nbs_client/src/components/User.js.jsx
+++ nbs_client/src/components/User.js.jsx
@@ -1,11 +1,23 @@
 var React = require('react');
 
 var User = React.createClass({
+  
+  
+  
+  
   render() {
+    const user_name = this.props.user;
     return (
       <div>
-            <h2>User Page</h2>
-            <div></div>
+            <h2>{user_name}</h2>
+              <div className="fb-like" 
+                data-href="http://www.your-domain.com/your-page.html" 
+                data-layout="standard" 
+                data-action="like" 
+                data-show-faces="true">
+              </div>
+            <div>
+            </div>
       </div>
     );
   }
