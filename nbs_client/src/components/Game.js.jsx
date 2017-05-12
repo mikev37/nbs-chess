b386ae2c12a2637d30700bca87c89e6163b08a62
Mikhail Vorontsov <mi17@umbc.edu>
2017-05-12 17:03:17 +0000

    Play screen now displays game!

--- nbs_client/src/components/Game.js.jsx
+++ nbs_client/src/components/Game.js.jsx
@@ -1,15 +1,108 @@
 import React, { Component } from 'react';
+import ChessPiece from './ChessPiece.js.jsx';
 
-class Game extends Component {
+var Game = React.createClass({
+  componentWillMount() {
+    this.state =  { loaded:false,game:null };
+    this.load();
+  },
+  load(){
+    var that = this;
+    fetch("/service/game/get" , {
+	        method: 'POST',
+	        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+	        body: JSON.stringify({
+	          'game_id': that.props.params.gameid,
+	        })
+      })
+      .then(function(data){
+          return data.json();
+      })
+      .then(function(data){
+        if(data){
+        console.log(data);
+        that.setState({loaded:true,game:data});
+        
+        }
+      })
+      .catch(function(err){
+        console.log('Send Error (.Y.)', err);  
+      })
+  },
+  
   render() {
     var path = this.props.params.gameid;
+    
+    if(this.state.loaded){
+      path = <GameBoard board_state={this.state.game.board_state}/>
+    }
+    
     return (
       <div>
         <p>Handling Game</p>
-        {path}
+        <div className="row">
+          <div className="col-md-6">
+          {path}
+          </div>
+          <div className="col-md-6">
+          
+          </div>
+        </div>
       </div>
     );
   }
-}
+});
+
+var GameBoard = React.createClass({
+  render() {
+    const board_state = this.props.board_state;
+    
+    var board_display = []
+    
+    for(var i = 0; i < board_state.length; i++){
+      var board_row = [];
+      for(var j = 0; j < board_state[0].length; j++){
+        var tile = board_state[i][j];
+        
+        board_row.push(<Tile color={tile.tile} piece={tile.piece} owner={tile.owner}/>)
+      }
+      board_display.push(<div className="board-row">{board_row}</div>);
+    }
+    
+    
+    return (
+      <div className="board">
+        {board_display}
+      </div>
+    );
+  }
+  
+});
+
+
+var Tile = React.createClass({
+  render() {
+    const color = this.props.color;
+    const piece = this.props.piece;
+    const piece_color = this.props.owner;
+    var divStyle = {
+      background:"lightgrey"
+    }
+    if(color === "Black"){
+      divStyle.background = "darkgrey";
+    }
+    
+    var figure = "";
+    if(piece !== "Empty"){
+      figure = <ChessPiece piece={piece} color={piece_color}/>;
+    }
+    
+    return (
+      <div style={divStyle} className="sqr">
+        <h2 className="piece">{figure}</h2>
+      </div>
+    );
+  }
+});
 
 export default Game;
---
+++ nbs_client/src/components/GameList.js.jsx
@@ -0,0 +1,179 @@
+var React = require('react');
+import ChessPiece from './ChessPiece.js.jsx';
+import { browserHistory } from 'react-router';
+
+var GameList = React.createClass({
+  render() {
+    var g_list = [];
+    var in_list = this.props.in_list;
+    for(var i = 0; i  < in_list.length;i++){
+      g_list.push(<GameShortCut usr={this.props.usr} id={in_list[i].game_id}/>);
+      
+    }
+    return (
+      <div className="sidebar">
+        <div className="list-group">
+            {g_list}
+        </div>
+      </div>
+    );
+  }
+});
+
+
+var GameShortCut = React.createClass({
+  componentWillMount() {
+    this.state =  { open:false,loaded:false,game:null };
+    this.load();
+  },
+  toGame(){
+    console.log("chemically calm");
+    browserHistory.push('/game/'+this.props.id);
+  },
+  toOpen() {
+    this.setState({open:!this.state.open});
+  },
+  load(){
+    var that = this;
+    fetch("/service/game/get" , {
+	        method: 'POST',
+	        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+	        body: JSON.stringify({
+	          'game_id': that.props.id,
+	        })
+      })
+      .then(function(data){
+          return data.json();
+      })
+      .then(function(data){
+        if(data){
+        that.setState({loaded:true,game:data});
+        console.log(data);
+        }
+      })
+      .catch(function(err){
+        console.log('Send Error (.Y.)', err);  
+      })
+  },
+  
+  drawLoading(){
+    return (
+    <div>
+      {this.props.id}
+      <div className="progress">
+        <div className="progress-bar progress-bar-striped active" role="progressbar"
+        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          Loading
+        </div>
+      </div>
+    </div>);
+  },
+  
+  drawPieces(arr,color){
+      var taken = [];
+      for(var i = 0; i < arr.length;i++)
+      {
+        taken.push(<ChessPiece piece={arr[i].name} color={color}/>);
+      }
+      return taken;
+  },
+  
+  render() {
+    var display = this.drawLoading();
+    
+    if(this.state.loaded){
+      const game = this.state.game;
+      const user = this.props.usr;
+      if(this.state.open){
+        
+        var bar = <span></span>
+        
+        if(game.black_player == null && game.white_player == null)
+        {
+          bar = 
+          <button className="btn btn-block" onClick={this.toGame}>
+            No Players have joined
+            <span className="pull-right glyphicon glyphicon-log-in"></span>
+          </button>
+        }
+        else if(game.black_player == null){
+            bar = 
+            <button className="btn btn-block" onClick={this.toGame}>
+                No White Player
+                <span className="pull-right glyphicon glyphicon-log-in"></span>
+            </button>;
+        }
+        else if(game.white_player == null){
+            bar = 
+            <button className="btn btn-block" onClick={this.toGame}>
+                No Black Player
+                <span className="pull-right glyphicon glyphicon-log-in"></span>
+            </button>
+        }
+        else{
+          var white_taken = this.drawPieces(game.white_captured,"White");
+          var black_taken = this.drawPieces(game.black_captured,"Black");
+          var warning = "";
+          
+          if(game.check_black){
+            warning += "\nBlack is in Check";
+          }
+          if(game.check_white){
+            warning += "\nWhite is in Check";
+          }
+          if(game.is_white){
+            if(game.white_player === user._id){
+              warning+= "\nIt is your turn";
+            }
+          }
+          else{
+            if(game.black_player === user._id){
+              warning+= "\nIt is your turn";
+            }
+          }
+          
+          var taken = "Taken: ";
+          
+          if(white_taken.length === 0 && black_taken.length === 0 ){
+            taken = "";
+          }
+          
+          bar = 
+            <button className="btn btn-block" onClick={this.toGame}>
+              {taken}{white_taken}{black_taken}
+              {warning}
+              <span className="pull-right glyphicon glyphicon-log-in"></span>
+            </button>
+        }
+        display = 
+          <div onClick={this.toOpen}>
+            <span className="pull-left glyphicon glyphicon-expand"></span>
+            {game.name} : Turn {game.turn_num}
+            <div>
+              {bar}
+            </div>
+          </div>;
+      }
+      else{
+      display = 
+      <div  onClick={this.toOpen}>
+      <span className="pull-left glyphicon glyphicon-collapse-down"></span>
+        {game.name} : Turn {game.turn_num}
+      </div>;
+      }
+      /*
+              <div>{white_taken}</div>
+        {game.name} : Turn {game.turn_num}
+        <div>{black_taken}</div>
+      
+      */
+    }
+    return (
+      <div className="list-group-item">
+            {display}
+      </div>
+    );
+  }
+});
+
+export default GameList;
